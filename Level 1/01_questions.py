7d92533f7c0168d5d299199dc56cd445f14176f2
SanjanaKannan-R <srirenugadevisanjana@gmail.com>
2026-08-27 11:56:48 +0530

    Problem solving

--- Level 1/01_questions.py
+++ Level 1/01_questions.py
@@ -1,6 +1,11 @@
 '''Get a number from user and add 2 to that number and print the result.'''
 
-number=int(input())
-result = number+2
-print(result)
+def add_numbers():
+    try:
+        number=int(input())
+        result = number+2
+        print(result)
+    except ValueError:
+        print("Error")
+add_numbers()
 
--- Level 1/02_questions.py
+++ Level 1/02_questions.py
@@ -1,5 +1,11 @@
 ''' Get a number from user and subtract 5 to that number and print the result.'''
 
-number = int(input())
-difference = number - 5
-print(difference)
+
+def subtract_number():
+    try:
+        number = int(input())
+        difference = number - 5
+        print(difference)
+    except ValueError:
+        print("Error")
+subtract_number()
